39d7156d1a2dc6779a18f34ee77f972d325d2dc2
justin <justins1707@gmail.com>
2018-01-27 17:11:11 +0800

    update

---
+++ terra-api/src/main/java/com/babel/terra/TerraServer.java
@@ -0,0 +1,10 @@
+package com.babel.terra;
+
+public class TerraServer {
+    /**
+     * 服务名
+     */
+    public static final String SERVER_NAME="terra";
+
+    public static final String USER_GET_IP="/api/user/get/ip";
+}
